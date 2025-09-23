18bd84a9c7e77d2d995f0b8b6123a92c63029331
LearningCode12222 <glazedaddon@gmail.com>
2025-09-23 21:10:34 +0100

    PopCounter.java

--- src/main/java/com/nnpg/glazed/modules/pvp/PopCounter.java
+++ src/main/java/com/nnpg/glazed/modules/pvp/PopCounter.java
@@ -1,37 +1,53 @@
 package com.nnpg.glazed.modules.pvp;
 
+import com.nnpg.glazed.GlazedAddon;
+import meteordevelopment.meteorclient.events.packets.PacketEvent;
+import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.orbit.EventHandler;
-import meteordevelopment.meteorclient.events.packets.PacketEvent;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.packet.s2c.play.EntityStatusS2CPacket;
+
 import java.util.HashMap;
 import java.util.Map;
-import com.nnpg.glazed.GlazedAddon;
+import java.util.UUID;
 
-public class PopCounter extends Module {
-    private final Map<Integer, Integer> pops = new HashMap<>();
+public class TotemPopCounter extends Module {
+    private final SettingGroup sg = settings.getDefaultGroup();
 
-    public PopCounter() {
-        super(GlazedAddon.pvp, "pop-counter", "Tracks how many times players have popped totems.");
+    private final Setting<Boolean> announce = sg.add(new BoolSetting.Builder()
+        .name("announce")
+        .description("Send chat message when someone pops a totem.")
+        .defaultValue(true).build()
+    );
+
+    private final Map<UUID, Integer> pops = new HashMap<>();
+
+    public TotemPopCounter() {
+        super(GlazedAddon.pvp, "totem-pop-counter", "Counts how many totems players have popped.");
     }
 
     @EventHandler
-    private void onPacketReceive(PacketEvent.Receive event) {
+    private void onPacket(PacketEvent.Receive event) {
         if (!(event.packet instanceof EntityStatusS2CPacket packet)) return;
-        if (packet.getStatus() != 35) return; // 35 = Totem pop
+        if (packet.getStatus() != 35) return; // 35 = totem pop
 
-        Entity entity = packet.getEntity(mc.world);
-        if (entity == null || entity == mc.player) return;
-
-        int id = entity.getId();
-        pops.put(id, pops.getOrDefault(id, 0) + 1);
-
-        info(entity.getName().getString() + " has popped " + pops.get(id) + " totem(s).");
+        Entity e = packet.getEntity(mc.world);
+        if (e instanceof PlayerEntity p) {
+            UUID id = p.getUuid();
+            pops.put(id, pops.getOrDefault(id, 0) + 1);
+            int count = pops.get(id);
+            if (announce.get()) info("%s popped a totem! (total: %d)", p.getName().getString(), count);
+        }
     }
 
     @Override
-    public void onDeactivate() {
-        pops.clear();
+    public String getInfoString() {
+        return String.valueOf(pops.values().stream().mapToInt(Integer::intValue).sum());
+    }
+
+    public Map<UUID, Integer> getPops() {
+        return new HashMap<>(pops);
     }
 }
