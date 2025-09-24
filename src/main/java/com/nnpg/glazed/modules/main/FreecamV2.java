aa6619a0d13c1c11be34148e045fcc5484cd5f73
LearningCode12222 <glazedaddon@gmail.com>
2025-09-24 12:19:09 +0100

    FreecamV2.java

--- src/main/java/com/nnpg/glazed/modules/main/FreecamV2.java
+++ src/main/java/com/nnpg/glazed/modules/main/FreecamV2.java
@@ -1,18 +1,14 @@
 package com.nnpg.glazed.modules.main;
 
 import com.nnpg.glazed.GlazedAddon;
-import meteordevelopment.meteorclient.events.entity.player.InteractEvent;
 import meteordevelopment.meteorclient.events.render.Render3DEvent;
 import meteordevelopment.meteorclient.events.world.TickEvent;
 import meteordevelopment.meteorclient.settings.DoubleSetting;
-import meteordevelopment.meteorclient.settings.Settings;
 import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.client.network.OtherClientPlayerEntity;
 import net.minecraft.client.render.Camera;
-import net.minecraft.client.render.CameraSubmersionType;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.Vec3d;
 
@@ -38,7 +34,7 @@ public class FreecamV2 extends Module {
     public void onActivate() {
         if (mc.player == null || mc.world == null) return;
 
-        // Clone player for visual stand-in
+        // Clone player as stand-in
         clone = new OtherClientPlayerEntity(mc.world, mc.player.getGameProfile());
         clone.copyPositionAndRotation(mc.player);
         clone.headYaw = mc.player.headYaw;
@@ -72,13 +68,15 @@ public class FreecamV2 extends Module {
 
         Vec3d forwardVec = Vec3d.fromPolar(0, camYaw).normalize();
         Vec3d strafeVec = Vec3d.fromPolar(0, camYaw + 90).normalize();
-        Vec3d move = forwardVec.multiply(forward).add(strafeVec.multiply(strafe)).normalize().multiply(speed.get());
+        Vec3d move = forwardVec.multiply(forward).add(strafeVec.multiply(strafe));
 
+        if (move.lengthSquared() > 0) move = move.normalize().multiply(speed.get());
         camPos = camPos.add(move);
+
         if (mc.options.jumpKey.isPressed()) camPos = camPos.add(0, speed.get(), 0);
         if (mc.options.sneakKey.isPressed()) camPos = camPos.add(0, -speed.get(), 0);
 
-        // Update camera rotation
+        // Lock yaw/pitch to your current mouse movement
         camYaw = mc.player.getYaw();
         camPitch = mc.player.getPitch();
     }
@@ -90,11 +88,5 @@ public class FreecamV2 extends Module {
             camera.setPos(camPos.x, camPos.y, camPos.z);
             camera.setRotation(camYaw, camPitch);
         }
-    }
-
-    @EventHandler
-    private void onInteract(InteractEvent event) {
-        // Block all interactions while freecamming
-        event.cancel();
     }
 }
