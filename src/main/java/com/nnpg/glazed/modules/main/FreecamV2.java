1fcb3b9e5b09069a79b2fcd97dc04f3d46fbc622
LearningCode12222 <glazedaddon@gmail.com>
2025-09-24 01:28:31 +0100

    FreecamV2.java

--- src/main/java/com/nnpg/glazed/modules/main/FreecamV2.java
+++ src/main/java/com/nnpg/glazed/modules/main/FreecamV2.java
@@ -1,39 +1,17 @@
 package com.nnpg.glazed.modules.main;
 
 import com.nnpg.glazed.GlazedAddon;
-import meteordevelopment.meteorclient.events.game.GameLeftEvent;
-import meteordevelopment.meteorclient.events.game.OpenScreenEvent;
-import meteordevelopment.meteorclient.events.meteor.KeyEvent;
-import meteordevelopment.meteorclient.events.meteor.MouseButtonEvent;
-import meteordevelopment.meteorclient.events.meteor.MouseScrollEvent;
 import meteordevelopment.meteorclient.events.packets.PacketEvent;
-import meteordevelopment.meteorclient.events.world.ChunkOcclusionEvent;
 import meteordevelopment.meteorclient.events.world.TickEvent;
-import meteordevelopment.meteorclient.settings.BoolSetting;
-import meteordevelopment.meteorclient.settings.DoubleSetting;
-import meteordevelopment.meteorclient.settings.Setting;
-import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
-import meteordevelopment.meteorclient.systems.modules.Modules;
-import meteordevelopment.meteorclient.systems.modules.movement.GUIMove;
-import meteordevelopment.meteorclient.utils.Utils;
-import meteordevelopment.meteorclient.utils.misc.input.Input;
-import meteordevelopment.meteorclient.utils.misc.input.KeyAction;
-import meteordevelopment.meteorclient.utils.player.Rotations;
 import meteordevelopment.orbit.EventHandler;
-import meteordevelopment.orbit.EventPriority;
+import net.minecraft.client.network.OtherClientPlayerEntity;
 import net.minecraft.client.option.Perspective;
-import net.minecraft.entity.Entity;
-import net.minecraft.network.packet.s2c.play.DeathMessageS2CPacket;
-import net.minecraft.network.packet.s2c.play.HealthUpdateS2CPacket;
-import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.hit.EntityHitResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 import net.minecraft.util.math.Vec3d;
-import org.joml.Vector3d;
-import org.lwjgl.glfw.GLFW;
 
 public class FreecamV2 extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
@@ -41,90 +19,14 @@ public class FreecamV2 extends Module {
     private final Setting<Double> speed = sgGeneral.add(new DoubleSetting.Builder()
         .name("speed")
         .description("Your speed while in freecam.")
-        .onChanged(aDouble -> speedValue = aDouble)
         .defaultValue(1.0)
-        .min(0.0)
+        .min(0.1)
         .build()
     );
 
-    private final Setting<Double> speedScrollSensitivity = sgGeneral.add(new DoubleSetting.Builder()
-        .name("speed-scroll-sensitivity")
-        .description("Allows you to change speed value using scroll wheel. 0 to disable.")
-        .defaultValue(0)
-        .min(0)
-        .sliderMax(2)
-        .build()
-    );
-
-    private final Setting<Boolean> staySneaking = sgGeneral.add(new BoolSetting.Builder()
-        .name("stay-sneaking")
-        .description("If you are sneaking when you enter freecam, whether your player should remain sneaking.")
-        .defaultValue(true)
-        .build()
-    );
-
-    private final Setting<Boolean> toggleOnDamage = sgGeneral.add(new BoolSetting.Builder()
-        .name("toggle-on-damage")
-        .description("Disables freecam when you take damage.")
-        .defaultValue(false)
-        .build()
-    );
-
-    private final Setting<Boolean> toggleOnDeath = sgGeneral.add(new BoolSetting.Builder()
-        .name("toggle-on-death")
-        .description("Disables freecam when you die.")
-        .defaultValue(false)
-        .build()
-    );
-
-    private final Setting<Boolean> toggleOnLog = sgGeneral.add(new BoolSetting.Builder()
-        .name("toggle-on-log")
-        .description("Disables freecam when you disconnect from a server.")
-        .defaultValue(true)
-        .build()
-    );
-
-    private final Setting<Boolean> reloadChunks = sgGeneral.add(new BoolSetting.Builder()
-        .name("reload-chunks")
-        .description("Disables cave culling.")
-        .defaultValue(true)
-        .build()
-    );
-
-    private final Setting<Boolean> renderHands = sgGeneral.add(new BoolSetting.Builder()
-        .name("show-hands")
-        .description("Whether or not to render your hands in freecam.")
-        .defaultValue(true)
-        .build()
-    );
-
-    private final Setting<Boolean> rotate = sgGeneral.add(new BoolSetting.Builder()
-        .name("rotate")
-        .description("Rotates to the block or entity you are looking at.")
-        .defaultValue(false)
-        .build()
-    );
-
-    private final Setting<Boolean> staticView = sgGeneral.add(new BoolSetting.Builder()
-        .name("static")
-        .description("Disables settings that move the view.")
-        .defaultValue(true)
-        .build()
-    );
-
-    public final Vector3d pos = new Vector3d();
-    public final Vector3d prevPos = new Vector3d();
-
-    private Perspective perspective;
-    private double speedValue;
-
-    public float yaw, pitch;
-    public float lastYaw, lastPitch;
-
-    private double fovScale;
-    private boolean bobView;
-
-    private boolean forward, backward, right, left, up, down, isSneaking;
+    private OtherClientPlayerEntity dummy;
+    private Perspective oldPerspective;
+    private PlayerEntity oldCamera;
 
     public FreecamV2() {
         super(Categories.Render, "freecam-v2", "Allows the camera to move away from the player.");
@@ -132,75 +34,74 @@ public class FreecamV2 extends Module {
 
     @Override
     public void onActivate() {
-        fovScale = mc.options.getFovEffectScale().getValue();
-        bobView = mc.options.getBobView().getValue();
-        if (staticView.get()) {
-            mc.options.getFovEffectScale().setValue((double) 0);
-            mc.options.getBobView().setValue(false);
-        }
-        yaw = mc.player.getYaw();
-        pitch = mc.player.getPitch();
+        if (mc.player == null || mc.world == null) return;
 
-        perspective = mc.options.getPerspective();
-        speedValue = speed.get();
+        // save old perspective + camera
+        oldPerspective = mc.options.getPerspective();
+        oldCamera = mc.cameraEntity instanceof PlayerEntity ? (PlayerEntity) mc.cameraEntity : null;
 
-        Utils.set(pos, mc.gameRenderer.getCamera().getPos());
-        Utils.set(prevPos, mc.gameRenderer.getCamera().getPos());
+        // create dummy clone of player (so body stays visible)
+        dummy = new OtherClientPlayerEntity(mc.world, mc.getSession().getProfile());
+        dummy.copyPositionAndRotation(mc.player);
+        dummy.setHeadYaw(mc.player.getHeadYaw());
+        mc.world.addEntity(dummy.getId(), dummy);
 
-        if (mc.options.getPerspective() == Perspective.THIRD_PERSON_FRONT) {
-            yaw += 180;
-            pitch *= -1;
-        }
-
-        lastYaw = yaw;
-        lastPitch = pitch;
-
-        isSneaking = mc.options.sneakKey.isPressed();
-
-        forward = Input.isPressed(mc.options.forwardKey);
-        backward = Input.isPressed(mc.options.backKey);
-        right = Input.isPressed(mc.options.rightKey);
-        left = Input.isPressed(mc.options.leftKey);
-        up = Input.isPressed(mc.options.jumpKey);
-        down = Input.isPressed(mc.options.sneakKey);
-
-        unpress();
-        if (reloadChunks.get()) mc.worldRenderer.reload();
+        // switch camera to dummy
+        mc.cameraEntity = dummy;
     }
 
     @Override
     public void onDeactivate() {
-        if (reloadChunks.get()) {
-            mc.execute(mc.worldRenderer::reload);
+        if (mc.player == null || mc.world == null) return;
+
+        // restore camera
+        if (oldCamera != null) mc.cameraEntity = oldCamera;
+        mc.options.setPerspective(oldPerspective);
+
+        // remove dummy
+        if (dummy != null) {
+            mc.world.removeEntity(dummy.getId(), net.minecraft.entity.Entity.RemovalReason.DISCARDED);
+            dummy = null;
         }
-
-        mc.options.setPerspective(perspective);
-
-        if (staticView.get()) {
-            mc.options.getFovEffectScale().setValue(fovScale);
-            mc.options.getBobView().setValue(bobView);
-        }
-
-        isSneaking = false;
     }
 
     @EventHandler
-    private void onOpenScreen(OpenScreenEvent event) {
-        unpress();
+    private void onTick(TickEvent.Post event) {
+        if (dummy == null) return;
 
-        prevPos.set(pos);
-        lastYaw = yaw;
-        lastPitch = pitch;
+        double s = speed.get();
+        Vec3d forward = Vec3d.fromPolar(0, mc.player.getYaw());
+        Vec3d right = Vec3d.fromPolar(0, mc.player.getYaw() + 90);
+
+        double velX = 0, velY = 0, velZ = 0;
+
+        if (mc.options.forwardKey.isPressed()) {
+            velX += forward.x * s;
+            velZ += forward.z * s;
+        }
+        if (mc.options.backKey.isPressed()) {
+            velX -= forward.x * s;
+            velZ -= forward.z * s;
+        }
+        if (mc.options.rightKey.isPressed()) {
+            velX += right.x * s;
+            velZ += right.z * s;
+        }
+        if (mc.options.leftKey.isPressed()) {
+            velX -= right.x * s;
+            velZ -= right.z * s;
+        }
+        if (mc.options.jumpKey.isPressed()) velY += s;
+        if (mc.options.sneakKey.isPressed()) velY -= s;
+
+        dummy.updatePosition(dummy.getX() + velX, dummy.getY() + velY, dummy.getZ() + velZ);
     }
 
-    private void unpress() {
-        mc.options.forwardKey.setPressed(false);
-        mc.options.backKey.setPressed(false);
-        mc.options.rightKey.setPressed(false);
-        mc.options.leftKey.setPressed(false);
-        mc.options.jumpKey.setPressed(false);
-        mc.options.sneakKey.setPressed(false);
+    @EventHandler
+    private void onPacketSend(PacketEvent.Send event) {
+        // block movement packets so server doesn’t rubberband
+        if (event.packet instanceof PlayerMoveC2SPacket) {
+            event.cancel();
+        }
     }
-
-    // the rest of the logic (onTick, key handling, etc.) stays exactly the same as in your pasted code.
 }
